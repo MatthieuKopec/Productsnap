c1c4f8c23e396aa0c8d7c9ace31948a56513dbfb
kopec_m <matkopec@gmail.com>
2015-03-31 00:02:42 +0200

    made input field the same size + semi opacity

--- Productsnap/app/src/main/java/com/staging/shotgun/productsnap/MainActivity.java
+++ Productsnap/app/src/main/java/com/staging/shotgun/productsnap/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends Activity {
 
     private void execTask() {
         UploadFileTask task = new UploadFileTask(this);
-        task.execute();
+        //task.execute();
     }
 
     private void askTitleImage() {
